fb723ff26a1251803610b8cd09c55a328581428b
lookermetaverse-dot <looker.metaverse@gmail.com>
2026-02-24 23:00:25 +0530

    Database-sql

--- routes/channels.php
+++ routes/channels.php
@@ -17,8 +17,8 @@ use Illuminate\Support\Facades\Broadcast;
     return (int) $user->id === (int) $id;
 }); */
 
-if (function_exists('getAppSettings')&&(getAppSettings('pusher_app_id'))) {
-    
+if (!app()->runninginconsole() && function_exists('getAppSettings') && (getAppSettings('pusher_app_id'))) {
+
     Broadcast::channel('vendor-channel.{vendorUid}', function ($user, $vendorUid) {
         // return true;
         return $vendorUid == getVendorUid();
